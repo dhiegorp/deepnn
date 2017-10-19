37623c4a2de2600a00e993de02e758dd7f2664bb
dhiegorp <dhiegorp@gmail.com>
2017-10-19 08:51:33 -0200

    lll

---
+++ remove_outputs.sh
@@ -0,0 +1,49 @@
+cd ~
+rm ~/executed/2layers/unigram/*
+rm ~/executed/3layers/unigram/*
+rm ~/executed/4layers/unigram/*
+rm ~/executed/5layers/unigram/*
+rm ~/executed/6layers/unigram/*
+rm ~/executed/7layers/unigram/*
+rm ~/executed/8layers/unigram/*
+rm ~/executed/9layers/unigram/*
+rm ~/executed/10layers/unigram/*
+
+rm ~/reports/2layers/unigram/*
+rm ~/reports/3layers/unigram/*
+rm ~/reports/4layers/unigram/*
+rm ~/reports/5layers/unigram/*
+rm ~/reports/6layers/unigram/*
+rm ~/reports/7layers/unigram/*
+rm ~/reports/8layers/unigram/*
+rm ~/reports/9layers/unigram/*
+rm ~/reports/10layers/unigram/*
+
+rm ~/checkpoints/2layers/unigram/*
+rm ~/checkpoints/3layers/unigram/*
+rm ~/checkpoints/4layers/unigram/*
+rm ~/checkpoints/5layers/unigram/*
+rm ~/checkpoints/6layers/unigram/*
+rm ~/checkpoints/7layers/unigram/*
+rm ~/checkpoints/8layers/unigram/*
+rm ~/checkpoints/9layers/unigram/*
+rm ~/checkpoints/10layers/unigram/*
+
+cd ~/checkpoints/2layers/unigram/
+rm -r *
+cd ~/checkpoints/3layers/unigram/
+rm -r *
+cd ~/checkpoints/4layers/unigram/
+rm -r *
+cd ~/checkpoints/5layers/unigram/
+rm -r *
+cd ~/checkpoints/6layers/unigram/
+rm -r *
+cd ~/checkpoints/7layers/unigram/
+rm -r *
+cd ~/checkpoints/8layers/unigram/
+rm -r *
+cd ~/checkpoints/9layers/unigram/
+rm -r *
+cd ~/checkpoints/10layers/unigram/
+rm -r *
