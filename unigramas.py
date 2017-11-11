90b159f6b0f5cbf4d6ad24080aa9dc66137c75cb
dhiegorp <dhiegorp@gmail.com>
2017-11-11 08:52:18 -0200

    logging

--- unigramas.py
+++ unigramas.py
@@ -15,8 +15,8 @@ def load_datasource(source, config):
 def configure_trials(name_pattern, data, config, hidden_layers):
 	trials =[]
 	for i, hidden_layer in enumerate(hidden_layers):
-		print('ae_bigrama_1L_',i, ' - hidden:', hidden_layer[0])
-		print([96].extend(hidden_layer))
+		print('ae_bigrama_1L_',i, ' - hidden:', hidden_layer)
+		print('qd? ', [96].extend(hidden_layer))
 		trials.append( Trial('ae_bigrama_1L_' + str(i), [96].extend(hidden_layer), data=data, config_dict=config) )
 	return trials
 
