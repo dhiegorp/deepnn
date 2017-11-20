3f9e414581fda6089a184273a5dea1a00b3afa3c
dhiegorp <dhiegorp@gmail.com>
2017-11-20 01:33:32 -0200

    updating bigram model to run for just one epoch

--- GLOBAL_EXP_CONFIG_1L_BIGRAM.py
+++ GLOBAL_EXP_CONFIG_1L_BIGRAM.py
@@ -24,7 +24,7 @@ GLOBAL = {
 	
 
 	'data_target_list' : [1,2,3,4,5,6,7,8,9],
-	'epochs': 200,
+	'epochs': 1,
 	'batch': 32,
 	'store_history' : True,
 	'shuffle_batches' : True,
@@ -54,17 +54,17 @@ MAP_DIMS = {
 
 def get_ae_callbacks(network_name):
 	ae_callbacks = [
-		EarlyStopping(monitor='val_loss', min_delta=0.01, patience=100, verbose=1, mode='min'),
+		#EarlyStopping(monitor='val_loss', min_delta=0.01, patience=100, verbose=1, mode='min'),
 		ModelCheckpoint(GLOBAL['checkpoints_dir'] + network_name + '.h5', monitor='val_loss', save_best_only=True, verbose=1), 
-		TensorBoard(log_dir=GLOBAL['tensorflow_dir'] + network_name , histogram_freq=1, write_graph=False)	
+		TensorBoard(log_dir=GLOBAL['tensorflow_dir'] + network_name , histogram_freq=0, write_graph=False)	
 	]
 	return ae_callbacks
 
 def get_mlp_callbacks(network_name):
 
 	mlp_callbacks = [
-		EarlyStopping(monitor='acc', min_delta=0.01, patience=100, verbose=1, mode='max'),
+		#EarlyStopping(monitor='acc', min_delta=0.01, patience=100, verbose=1, mode='max'),
 		ModelCheckpoint(GLOBAL['checkpoints_dir'] + network_name + '_mlp.h5', monitor='val_acc', save_best_only=True, verbose=1), 
-		TensorBoard(log_dir=GLOBAL['tensorflow_dir'] + network_name + '_mlp', histogram_freq=1, write_graph=False)	
+		TensorBoard(log_dir=GLOBAL['tensorflow_dir'] + network_name + '_mlp', histogram_freq=0, write_graph=False)	
 	]
 	return mlp_callbacks
